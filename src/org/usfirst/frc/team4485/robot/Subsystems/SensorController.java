a2b8559d45aaf9d1eda98ba57e124e4829e694a1
Michael Collins <michaelcollins247@gmail.com>
2017-10-18 00:56:21 -0400

    Added power handler wrapper as a public sensor.

--- src/org/usfirst/frc/team4485/robot/Subsystems/SensorController.java
+++ src/org/usfirst/frc/team4485/robot/Subsystems/SensorController.java
@@ -1,6 +1,8 @@
 package org.usfirst.frc.team4485.robot.Subsystems;
 
 import org.usfirst.frc.team4485.robot.RobotIndexing;
+import org.usfirst.frc.team4485.robot.Subsystems.Systems.PowerHandlerSystem;
+
 import edu.wpi.first.wpilibj.AnalogInput;
 
 
@@ -10,6 +12,7 @@ public class SensorController {
 	
 	// Create your sensor objects here
 	public AnalogInput cameraDistanceSensor;
+	public PowerHandlerSystem powerHandlerSystem;
 	
 	// Create the value objects here. You will update these with update()
 	//private double gearOpticalSensorVal;
@@ -22,10 +25,12 @@ public class SensorController {
 	private void init() {
 		// Here is where you initialize sensors
 		cameraDistanceSensor = new AnalogInput(id.cameraDistanceSensor);
+		powerHandlerSystem = new PowerHandlerSystem();
+		powerHandlerSystem.setID(100);
 	}
 	
 	public void update() {
-		
+		powerHandlerSystem.update();
 	}
 	
 	public double getDistanceSensorDistance(AnalogInput sensor, double mV_in) {
