133ec85b6b9dd13de3b28d3dfd0cd87bd7ac3ed1
Michael Collins <michaelcollins247@gmail.com>
2017-10-19 20:43:46 -0400

    Added the AHRS.

--- src/org/usfirst/frc/team4485/robot/Subsystems/SensorController.java
+++ src/org/usfirst/frc/team4485/robot/Subsystems/SensorController.java
@@ -3,7 +3,16 @@ package org.usfirst.frc.team4485.robot.Subsystems;
 import org.usfirst.frc.team4485.robot.RobotIndexing;
 import org.usfirst.frc.team4485.robot.Subsystems.Systems.PowerHandlerSystem;
 
+import com.kauailabs.navx.frc.AHRS;
+
 import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.SPI;
+
+
+/*
+ * TODO:
+ * Add control variables and functions for the AHRS
+ */
 
 
 public class SensorController {
@@ -12,10 +21,12 @@ public class SensorController {
 	
 	// Create your sensor objects here
 	public AnalogInput cameraDistanceSensor;
+	public AHRS ahrs;
 	public PowerHandlerSystem powerHandlerSystem;
 	
 	// Create the value objects here. You will update these with update()
 	//private double gearOpticalSensorVal;
+	public boolean ahrsYawZeroed = false;
 	
 	private double leftDriveRPM = 0.0, rightDriveRPM = 0.0;
 	
@@ -27,8 +38,16 @@ public class SensorController {
 	private void init() {
 		// Here is where you initialize sensors
 		cameraDistanceSensor = new AnalogInput(id.cameraDistanceSensor);
+		
 		powerHandlerSystem = new PowerHandlerSystem();
 		powerHandlerSystem.setID(100);
+		
+		// Initialize the AHRS
+		try {
+			ahrs = new AHRS(SPI.Port.kMXP);
+		} catch (Exception ex) {
+			System.out.println("Warning: AHRS Error: " + ex.getMessage());
+		}
 	}
 	
 	public void update() {
