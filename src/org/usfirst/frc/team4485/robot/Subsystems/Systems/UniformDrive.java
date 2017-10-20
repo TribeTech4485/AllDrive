97cc035a814637a9c81f04a53735f1fe96304d69
Michael Collins <michaelcollins247@gmail.com>
2017-10-20 14:12:14 -0400

    Deprecated the old drive system.

--- src/org/usfirst/frc/team4485/robot/Subsystems/Systems/UniformDrive.java
+++ src/org/usfirst/frc/team4485/robot/Subsystems/Systems/UniformDrive.java
@@ -15,6 +15,10 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 
+// TODO: Remove this subsystem once the new one has been tested
+// This whole class is deprecated
+
+@Deprecated
 public class UniformDrive extends Subsystem {
 	public AHRS ahrs;
 	private PIDController pid;
