2efc9bf591d39adf0893d1d8ebd9dd40ce37167a
Michael Collins <michaelcollins247@gmail.com>
2018-01-16 21:05:17 -0500

    Updated TeleOpControl for new subsystems

--- src/org/usfirst/frc/team4485/robot/TeleOpControl.java
+++ src/org/usfirst/frc/team4485/robot/TeleOpControl.java
@@ -20,7 +20,7 @@ public class TeleOpControl {
 		userControl.updateControls();
 				
 		// Drive using the drive subsystem
-		//subsystems.driveSystem.drive4Motors(userControl.drive_leftStickY, userControl.drive_rightStickY);
+		subsystems.driveSystem.drive4Motors(userControl.drive_leftStickY, userControl.drive_rightStickY);
 		subsystems.driveSystem.drive4Motors(userControl.drive_leftStickY, userControl.drive_rightStickY); 	// Flip the input sticks for some robots
 		if (userControl.getRawDriveButton(id.d_shiftDown)) subsystems.shifterPneumaticSystem.shiftDown();
 		else if (userControl.getRawDriveButton(id.d_shiftUp)) subsystems.shifterPneumaticSystem.shiftUp();
@@ -32,36 +32,6 @@ public class TeleOpControl {
 		
 		// Make the drive controller rumble with drive amount
 		//userControl.rumbleController(id.driveController, (Math.abs(userControl.drive_leftStickY) + Math.abs(userControl.drive_rightStickY)) / 2);
-		
-		// Control the box pneumatics
-		// Cntrol the guide
-		if (userControl.getRawControlButton(id.c_guideOutButton)) subsystems.boxPneumaticSystem.setGuideOut(true);
-		else if (userControl.getRawControlButton(id.c_guideInButton)) subsystems.boxPneumaticSystem.setGuideOut(false);
-		
-		// Control the door
-		if (userControl.getRawControlButton(id.c_doorOutButton)) subsystems.boxPneumaticSystem.setDoorOut(true);
-		else if (userControl.getRawControlButton(id.c_doorInButton)) subsystems.boxPneumaticSystem.setDoorOut(false);
-		
-		// Control the pusher
-		// Only set it out here because the subsystem will bring it in when its clear
-		if (userControl.getRawControlButton(id.c_pusherOutButton)) subsystems.boxPneumaticSystem.setPusherOut(true);
-		
-		// Expel the gear
-		if (userControl.getRawControlButton(id.c_expelGearButton)) subsystems.boxPneumaticSystem.expel();
-		// Update the box pneumatic system
-		subsystems.boxPneumaticSystem.update();
-		
-		
-		// Control the lifter
-		subsystems.liftSystem.setLift(userControl.getRawControlButton(id.c_liftButton));
-		subsystems.liftSystem.setPower(userControl.getAxis(id.controlController, id.c_liftAxis));
-		subsystems.liftSystem.update();
-		
-		// Rumble the controllers depending on the state of the robot
-		//if (subsystems.boxPneumaticSystem.getDoorState()) userControl.rumbleController(id.controlController, 0.25);
-		//else userControl.rumbleController(id.controlController, 0);
-		if (subsystems.boxPneumaticSystem.getDoorState()) userControl.rumbleController(userControl.kRumbleDash, id.controlController);
-		else userControl.rumbleController(userControl.kRumbleNone, id.controlController);
 		
 		testPrint();
 	}
