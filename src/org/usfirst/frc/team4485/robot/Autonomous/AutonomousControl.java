508e7de8ceeef95b147a664ae3dda25fe6a73681
Michael Collins <michaelcollins247@gmail.com>
2018-01-16 21:04:55 -0500

    Updated Autonomous Controller for new autonomous layout

--- src/org/usfirst/frc/team4485/robot/Autonomous/AutonomousControl.java
+++ src/org/usfirst/frc/team4485/robot/Autonomous/AutonomousControl.java
@@ -13,38 +13,14 @@ public class AutonomousControl {
 	//  Don't run these programs, they do not exist
 	
 	AutoProgram example;
-	AutoProgram driveDistance;	// This one exists, go ahead and run it
-	AutoProgram turnToAngle;	// This one too
-	AutoProgram testSerial;		// And this one
-	AutoProgram ballChase;		// this one also
-	AutoProgram driveToPeg;		// Just don't use example, it's the only one that doesn't exist
+	
 	
 	AutoProgram zeroYaw;
 	
-	AutoProgram driveToZone;
-	AutoProgram placeGearForward;
-	
-	public AutonomousControl(SubsystemsControl subsystems) {	
-		driveDistance = new DriveDistance(subsystems);
-		turnToAngle = new TurnToAngle(subsystems);
-		testSerial = new TestSerial(subsystems);
-		ballChase = new BallChase(subsystems);
-		driveToPeg = new DriveToPeg(subsystems);
-		
-		zeroYaw = new ZeroYaw(subsystems);
-		
-		driveToZone = new DriveToZone(subsystems);
-		placeGearForward = new PlaceGearForward(subsystems);
-		
+	public AutonomousControl(SubsystemsControl subsystems) {
+				
 		autoChooser.addDefault("None", null);
 		autoChooser.addObject("Example", example);
-		autoChooser.addObject("Drive Distance", driveDistance);
-		autoChooser.addObject("Turn To Angle", turnToAngle);
-		autoChooser.addObject("Test Serial", testSerial);
-		autoChooser.addObject("Ball Chase", ballChase);
-		autoChooser.addObject("Drive To Peg", driveToPeg);
-		autoChooser.addObject("DriveToZone", driveToZone);
-		autoChooser.addObject("Place Gear Forward", placeGearForward);
 		
 		autoChooser.addObject("Zero Yaw", zeroYaw);
 		
