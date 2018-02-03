4119e979703768985bf84f7a26943a78c05c718a
Michael Collins <michaelcollins247@gmail.com>
2018-02-03 13:33:06 -0500

    I2C Test Complete Successfully

--- src/org/usfirst/frc/team4485/robot/Autonomous/I2CTestAuto.java
+++ src/org/usfirst/frc/team4485/robot/Autonomous/I2CTestAuto.java
@@ -7,7 +7,8 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class I2CTestAuto extends AutoProgram {
 
-	I2C port;
+	I2C i2c = new I2C(I2C.Port.kOnboard, 8);
+	byte[] byteToSend= new byte[1];
 	
 	public I2CTestAuto(SubsystemsControl _subsystems) {
 		super(_subsystems);
@@ -17,12 +18,25 @@ public class I2CTestAuto extends AutoProgram {
 	@Override
 	protected void init() {
 		// TODO Auto-generated method stub
-		port = new I2C(I2C.Port.kOnboard, 8);
+		for (int i = 0; i < 1; i++) {
+			byteToSend[i] = 0;
+		}
 	}
 
 	@Override
 	protected void run() {
+		byte[] byteRcvd = new byte[5];
+		for (int i = 0; i < 5; i++) {
+			byteRcvd[i] = 0;
+		}
+		byteRcvd[0] = 0;
+		i2c.transaction(byteToSend, 1, byteRcvd, 5);
 		
+		String rcvdString = "";
+		for (int i = 0; i < 5; i++) {
+			rcvdString += (char)byteRcvd[i];
+		}
+		
+		SmartDashboard.putString("Bytes RCVD", rcvdString);
 	}
-
 }
