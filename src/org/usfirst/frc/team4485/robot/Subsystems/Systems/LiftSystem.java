dc6dd8e436bf0c32f2d86f53b54ab2887b47f2f8
Michael Collins <michaelcollins247@gmail.com>
2018-02-20 20:08:11 -0500

    Implemented sensors with the lift system

--- src/org/usfirst/frc/team4485/robot/Subsystems/Systems/LiftSystem.java
+++ src/org/usfirst/frc/team4485/robot/Subsystems/Systems/LiftSystem.java
@@ -1,7 +1,9 @@
 package org.usfirst.frc.team4485.robot.Subsystems.Systems;
 
+import org.usfirst.frc.team4485.robot.Robot;
 import org.usfirst.frc.team4485.robot.Subsystems.Subsystem;
 
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 public class LiftSystem extends Subsystem{
@@ -11,6 +13,10 @@ public class LiftSystem extends Subsystem{
 		
 	private WPI_TalonSRX liftMotor;
 	
+	// Sensor values
+	private boolean encodersInitialized = false;
+	private double liftPosition = 0;
+	
 	@Override
 	protected void initSystem() {
 		liftMotor = new WPI_TalonSRX(id.liftMotor);
@@ -18,21 +24,56 @@ public class LiftSystem extends Subsystem{
 
 	@Override
 	protected void updateSystem() {
+		updateSensors();
 		updateMotorControl();
 	}
 
 	@Override
-	protected void killSystem() {}
+	protected void killSystem() {
+		setLift(0);
+		encodersInitialized = false;
+		update();
+	}
 
 	@Override
 	protected void errorHandler() {}
 	
-	public void moveTheLift(double lift) {
+	//// Public control functions
+	public void setLift(double lift) {
 		liftSpeed = lift;
+		if (lift > 0.1) lift = 0.1;
+		if (lift < -0.1) lift = -0.1;
 	}
+	public double getPosition() {
+		return liftPosition;
+	}
+	////
 	
 	private void updateMotorControl() {
+		if (liftSpeed <-0.5) liftSpeed = -0.5;
 		if (reverseInput) liftSpeed *= -1;		
 		liftMotor.set(liftSpeed);
 	}
+	
+	private void updateSensors() {
+		try {
+		if (!encodersInitialized) {
+			// Initialize encoders
+			liftMotor.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Absolute, 0, 10);
+			liftMotor.setSelectedSensorPosition(0,0,10);
+			encodersInitialized = true;
+		}
+			liftPosition = liftMotor.getSelectedSensorPosition(0);
+			
+			// Check the lower limit switch
+			boolean lowPosition = Robot.sensorController.getDigitalInput(id.liftLowerLimitSwitch);
+			if (lowPosition) {
+				liftMotor.setSelectedSensorPosition(0, 0, 10);
+				// reset the encoder to 0
+			}
+		} catch (Exception ex) {
+			
+		}
+	}
+	
 }
