69899ff9570bd101d57c6de42278aacc8e402676
Michael Collins <michaelcollins247@gmail.com>
2018-03-20 07:18:11 -0400

    Autonomous for testing the driveToDistanceStraight function in drive system

---
+++ src/org/usfirst/frc/team4485/robot/Autonomous/DriveStraightTest.java
@@ -0,0 +1,26 @@
+package org.usfirst.frc.team4485.robot.Autonomous;
+
+import org.usfirst.frc.team4485.robot.Subsystems.SubsystemsControl;
+
+public class DriveStraightTest extends AutoProgram {
+
+	public DriveStraightTest(SubsystemsControl _subsystems) {
+		super(_subsystems);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void init() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void run() {
+		// TODO Auto-generated method stub
+		subsystems.driveSystem.setBraking(true);
+		if (subsystems.driveSystem.driveToDistanceStraight(-381) < 1) auto_complete = true;
+		subsystems.driveSystem.update();
+	}
+
+}
