f907d9adf6e916e95f0d1989839babe34f76eb42
Michael <michaelcollins247@gmail.com>
2018-04-12 00:15:07 -0400

    Got RightAuto working

--- src/org/usfirst/frc/team4485/robot/Autonomous/RightAuto.java
+++ src/org/usfirst/frc/team4485/robot/Autonomous/RightAuto.java
@@ -13,18 +13,16 @@ public class RightAuto extends AutoProgram {
 
 	@Override
 	protected void init() {
-		subsystems.sensorController.zeroAHRSYaw();
+		//subsystems.sensorController.zeroAHRSYaw();
 	}
 
 	@Override
 	protected void run() {
 		subsystems.driveSystem.setBraking(true);
-		subsystems.shifterPneumaticSystem.shiftDown();
-		subsystems.liftSystem.setLiftPIDOverride(true);	// Set this to true if disabling the lift
+		subsystems.shifterPneumaticSystem.shiftUp();
+		subsystems.liftSystem.setLiftPIDOverride(false);	// Set this to true if disabling the lift
 		
-		if (gameSpecificMessage.charAt(1) == 'R') {
-			auto_complete = driveToScale();
-		} else if (gameSpecificMessage.charAt(0) == 'R') {
+		if (gameSpecificMessage.charAt(0) == 'R') {
 			auto_complete = driveToSwitch();
 		} else {
 			auto_complete = driveBaseLine();
@@ -33,7 +31,9 @@ public class RightAuto extends AutoProgram {
 		// Update the systems
 		subsystems.driveSystem.update();
 		subsystems.shifterPneumaticSystem.update();
+		subsystems.collectorSystem.update();
 		subsystems.liftSystem.update();
+		subsystems.shifterPneumaticSystem.shiftDown();
 	}
 	
 	double startTime = -1;
@@ -43,33 +43,39 @@ public class RightAuto extends AutoProgram {
 		switch(step) {
 		case 0:			
 			// Drive the distance to the scale
-			if (subsystems.driveSystem.driveToDistance(-838.2, 20000) < 1) step++;
+			if (subsystems.driveSystem.driveToDistanceStraight(-800) < 1) step++;
 			break;
 		case 1:
 			// Turn 90 degrees clockwise
-			if (subsystems.driveSystem.driveToAngle(-90) < 1) step++;
+			if (subsystems.driveSystem.driveToAngle(-105) < 1) {
+				subsystems.driveSystem.drive4Motors(0, 0);
+				step++;
+			}
+			subsystems.liftSystem.setLiftPosition_presetNum(6);
 			break;
 		case 2:
-			subsystems.liftSystem.setLiftPosition_presetNum(6);
 			if (subsystems.liftSystem.getLiftOffset() < 200) step++;
+			break;
 		case 3:
+			if (subsystems.driveSystem.driveToDistanceStraight(-20) < 1) step++;
+			break;
+		case 4:
 			// Expel the block
 			subsystems.collectorSystem.setExpel(true);
 			if (startTime < 0) startTime = System.currentTimeMillis();
 			SmartDashboard.putNumber("Time", System.currentTimeMillis() - startTime);
-			if (System.currentTimeMillis() - startTime > 250) {
+			if (System.currentTimeMillis() - startTime > 1000) {
 				subsystems.collectorSystem.setExpel(false);
 				startTime = -1;
 				step++;
 			}
 			break;
-		case 4:
+		case 5:
 			// Lower the lift
 			if (subsystems.liftSystem.homeLift()) step++;
 			break;
-		case 5:
-			return true;
 		}
+		if (step >= 6) return true;
 		return false;
 	}
 	
@@ -78,16 +84,17 @@ public class RightAuto extends AutoProgram {
 		switch (step) {
 		case 0:
 			subsystems.liftSystem.setLiftPosition_presetNum(3);
-			if (subsystems.driveSystem.driveToDistance(-365.76, 10000) < 1) step++;
+			if (subsystems.driveSystem.driveToDistanceStraight(-335.25/*-365.76*/) < 1) step++;
 			break;
 		case 1:
-			if (subsystems.driveSystem.driveToAngle(-90) < 1) step++;
+			if (subsystems.driveSystem.driveToAngle(-100) < 1) step++;
 			break;
 		case 2:
-			if (subsystems.driveSystem.driveToDistance(-121.96, 2000) < 1) step++;
+			if (subsystems.liftSystem.getLiftOffset() > 200) break;	// check if the lift is in the correct position (to avoid damage)
+			if (subsystems.driveSystem.driveToDistanceStraight(-121.96, 1000) < 1) step++;
 			break;
 		case 3:
-			// Expel into the switch
+			// Expel the block into the switch
 			subsystems.collectorSystem.setExpel(true);
 			if (startTime < 0) startTime = System.currentTimeMillis();
 			SmartDashboard.putNumber("Time", System.currentTimeMillis() - startTime);
@@ -96,23 +103,39 @@ public class RightAuto extends AutoProgram {
 				startTime = -1;
 				step++;
 			}
+			break;
 		case 4:
 			// Backup
-			if (subsystems.driveSystem.driveToDistance(91.92) < 1) step++;
+			subsystems.driveSystem.drive4Motors(0.5, 0.5);
+			if (startTime < 0) startTime = System.currentTimeMillis();
+			SmartDashboard.putNumber("Time", System.currentTimeMillis() - startTime);
+			if (System.currentTimeMillis() - startTime > 800) {
+				subsystems.driveSystem.drive4Motors(0,0);
+				startTime = -1;
+				step++;
+			}
 			break;
+			/*if (subsystems.driveSystem.driveToDistance(30) < 1) step++;
+			break;*/
+			/*double offset = subsystems.driveSystem.driveToDistanceStraight(25);
+			SmartDashboard.putNumber("Auto - Offset", offset);
+			if (offset < 1) step++;
+			break;*/
 		case 5:
 			// Home the lift
-			if (subsystems.liftSystem.homeLift())step++;
+			subsystems.driveSystem.drive4Motors(0, 0);
+			subsystems.driveSystem.setBraking(false);
 			subsystems.shifterPneumaticSystem.shiftDown();
-			return true;
+			if (subsystems.liftSystem.homeLift())step++;
+			break;
 		}
-		return true;
+		if (step >= 6) return true;
+		return false;
 	}
 	
 	private boolean driveBaseLine() {
 		// Drive across the base line
-		if (subsystems.driveSystem.driveToDistance(-365.76, 30000) < 1) return true;
+		if (subsystems.driveSystem.driveToDistanceStraight(-365.76, 30000) < 1) return true;
 		return false;
 	}
-
 }
