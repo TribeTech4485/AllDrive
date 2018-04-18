12e0b8943d72ef90e6f5422174e0387ca0f5c4a0
Michael <michaelcollins247@gmail.com>
2018-04-18 10:00:43 -0400

    Adjusted distances and turns

--- src/org/usfirst/frc/team4485/robot/Autonomous/CenterAuto.java
+++ src/org/usfirst/frc/team4485/robot/Autonomous/CenterAuto.java
@@ -48,7 +48,7 @@ public class CenterAuto extends AutoProgram {
 		case 0:
 			// Move the lift to the switch preset, drive forward
 			subsystems.liftSystem.setLiftPosition_presetNum(3);
-			if (subsystems.driveSystem.driveToDistance(-91.44) < 1) step++;
+			if (subsystems.driveSystem.driveToDistance(-75/*-91.44*/) < 1) step++;
 			break;
 		case 1:
 			// Turn the the left
@@ -63,12 +63,16 @@ public class CenterAuto extends AutoProgram {
 			if (subsystems.driveSystem.driveToAngle(0) < 1) step++;
 			break;
 		case 4:
+			subsystems.driveSystem.drive4Motors(0, 0);
+			step++;
+			break;
+		case 5:
 			if (subsystems.liftSystem.getLiftOffset() > 200) break;	// check if the lift is in the correct position (to avoid damage)
 			// Move forward to the switch with timeout so we can stop if we hit it early
 			// -121.92 cm	
-			if (subsystems.driveSystem.driveToDistance(-60.96,500) < 1) step++;	// Drive to distance with a timeout of 5000 ms
+			if (subsystems.driveSystem.driveToDistance(-70/*-60.96*/,1000) < 1) step++;	// Drive to distance with a timeout of 5000 ms
 			break;
-		case 5:
+		case 6:
 			// Expel into the switch
 			subsystems.collectorSystem.setExpel(true);
 			if (startTime < 0) startTime = System.currentTimeMillis();
@@ -78,16 +82,16 @@ public class CenterAuto extends AutoProgram {
 				startTime = -1;
 				step++;
 			}
-		case 6:
+		case 7:
 			// Backup
 			if (subsystems.driveSystem.driveToDistance(91.92) < 1) step++;
 			break;
-		case 7:
+		case 8:
 			subsystems.driveSystem.drive4Motors(0, 0);
 			// Home the lift
 			if (subsystems.liftSystem.homeLift()) step++;
 			break;
-		case 8:
+		case 9:
 			// Stop
 			subsystems.driveSystem.setBraking(false);
 			subsystems.shifterPneumaticSystem.shiftDown();
@@ -116,11 +120,15 @@ public class CenterAuto extends AutoProgram {
 			if (subsystems.driveSystem.driveToAngle(0) < 1) step++;
 			break;
 		case 4:
-			// Drive forward 60.96 cm (3 ft)
-			if (subsystems.liftSystem.getLiftOffset() > 200) break;	// check if the lift is in the correct position (to avoid damage)
-			if (subsystems.driveSystem.driveToDistance(-60.96, 500) < 1) step++;
+			subsystems.driveSystem.drive4Motors(0, 0);
+			step++;
 			break;
 		case 5:
+			// Drive forward 60.96 cm (3 ft)
+			if (subsystems.liftSystem.getLiftOffset() > 200) break;	// check if the lift is in the correct position (to avoid damage)
+			if (subsystems.driveSystem.driveToDistance(-70/*-60.96*/, 1000) < 1) step++;
+			break;
+		case 6:
 			// Expel the block into the switch
 			subsystems.collectorSystem.setExpel(true);
 			if (startTime < 0) startTime = System.currentTimeMillis();
@@ -131,16 +139,16 @@ public class CenterAuto extends AutoProgram {
 				step++;
 			}
 			break;
-		case 6:
+		case 7:
 			// Backup
 			if (subsystems.driveSystem.driveToDistance(91.44) < 1) step++;
 			break;
-		case 7:
+		case 8:
 			subsystems.driveSystem.drive4Motors(0, 0);
 			// Home the lift
 			if (subsystems.liftSystem.homeLift()) step++;
 			break;
-		case 8:
+		case 9:
 			// Stop
 			subsystems.driveSystem.setBraking(false);
 			subsystems.shifterPneumaticSystem.shiftDown();			
